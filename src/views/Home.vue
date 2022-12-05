a260828942ff3d897ed15c97485544660c59836d
sergei-1973 <baragozik00@gmail.com>
2022-12-06 02:16:49 +0300

    npm json-server + create singleprojecct

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,26 @@
 <template>
-  <div class="home">Home</div>
+  <div class="home">
+    <div v-for="project in projects" :key="project.id">
+      <SingleProject :project="project" />
+    </div>
+  </div>
 </template>
 
 <script>
+import SingleProject from "@/components/SingleProject.vue";
 export default {
   name: "HomeView",
-  components: {},
+  components: { SingleProject },
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+      .then((res) => res.json())
+      .then((data) => (this.projects = data))
+      .catch((err) => console.log(err.message));
+  },
 };
 </script>
