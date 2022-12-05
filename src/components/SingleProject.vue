d61389151254689dd86defbb62521788557c3a67
sergei-1973 <baragozik00@gmail.com>
2022-12-06 02:55:27 +0300

    добавил стили к commit

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="project">
+  <div class="project" :class="{ complete: project.complete }">
     <div class="actions">
       <h3 @click="showDetails = !showDetails">{{ project.title }}</h3>
       <div class="i">
         <span class="material-icons"> edit </span>
         <span class="material-icons" @click="deleteProject"> delete </span>
-        <span class="material-icons" @click="completeProject"> done </span>
+        <span class="material-icons done" @click="completeProject"> done </span>
       </div>
     </div>
     <div v-if="showDetails" class="details">
@@ -67,5 +67,11 @@ export default {
 }
 .actions .material-icons:hover {
   color: #777;
+}
+.project.complete {
+  border-left: 4px solid teal;
+}
+.project.complete .done {
+  color: teal;
 }
 </style>
