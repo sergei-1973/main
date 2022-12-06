a9257f14bc2a5ce275279b22ccc705916260fdc6
sergei-1973 <baragozik00@gmail.com>
2022-12-06 03:54:35 +0300

    created navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="main-navbar">
+    <router-link :to="{ name: 'home' }">Home</router-link>
+    <router-link :to="{ name: 'AddProject' }">Add Project</router-link>
+  </nav>
+</template>
+
+<style>
+.main-navbar {
+  text-align: center;
+  margin: 40px auto;
+}
+.main-navbar a {
+  display: inline-block;
+  text-decoration: none;
+  color: #999;
+  font-size: 18px;
+  margin: 0 10px;
+}
+a.router-link-active {
+  border-bottom: 2px solid teal;
+  padding-bottom: 4px;
+}
+</style>
